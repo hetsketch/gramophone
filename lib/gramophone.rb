3c48db8554cc526768b320ea80ac30896d6b0dae
Alexander Koval <alexander.koval@jetruby.com>
2017-05-18 12:48:55 +0300

    Init commit

---
+++ lib/gramophone.rb
@@ -0,0 +1,10 @@
+require "gramophone/version"
+
+module Gramophone
+  # Your code goes here...
+  class Clazz
+    def self.run
+      puts "Hello from gem"
+    end
+  end
+end
---
+++ lib/gramophone/version.rb
@@ -0,0 +1,3 @@
+module Gramophone
+  VERSION = "0.1.0"
+end
